6861726d25cb314174d12500ae3b9402f14b1c60
witnes sostenes <112706208+nshobairwe@users.noreply.github.com>
2024-12-13 07:55:23 +0300

    questions part done

---
+++ src/main/java/com/witnes/quizapp/controller/QuestionController.java
@@ -0,0 +1,36 @@
+package com.witnes.quizapp.controller;
+
+import com.witnes.quizapp.Question;
+import com.witnes.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("/by-category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+}
---
+++ src/main/java/com/witnes/quizapp/service/QuestionService.java
@@ -0,0 +1,58 @@
+package com.witnes.quizapp.service;
+
+
+import com.witnes.quizapp.Question;
+import com.witnes.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    private final QuestionDao questionDao;
+
+    @Autowired
+    public QuestionService(QuestionDao questionDao) {
+        this.questionDao = questionDao;
+    }
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            List<Question> questions = questionDao.findAll();
+            return new ResponseEntity<>(questions, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            List<Question> questions = questionDao.findByCategory(category);
+            if (questions.isEmpty()) {
+                return new ResponseEntity<>(questions, HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(questions, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try {
+            questionDao.save(question);
+            return new ResponseEntity<>("success", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("An error occurred while saving the question", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
